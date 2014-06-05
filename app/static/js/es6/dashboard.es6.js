2349d5310f1060bd3ef727f7292743551b7cd8ed
MaRisa Jackson <mhljackson@gmail.com>
2014-06-05 00:57:03 -0500

    waiting for css

--- app/static/js/es6/dashboard.es6.js
+++ app/static/js/es6/dashboard.es6.js
@@ -46,7 +46,6 @@ function ajax(url, type, data={}, success=r=>console.log(r), dataType='html'){
 
   function showEntries(){
     ajax('/entries/show', 'get', null, html=>{
-      console.log(html);
       $('#content').empty().append(html);
     });
   }
