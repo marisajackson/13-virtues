e30eeb39ef3dc97cbce9c162ce7c3436c005e9a4
Nathan Hood <nathanhood@me.com>
2014-06-04 23:08:00 -0500

    chart

--- app/routes/entries.js
+++ app/routes/entries.js
@@ -1,14 +1,15 @@
 'use strict';
 
-// var traceur = require('traceur');
-// var Entry = traceur.require(__dirname + '/../../app/models/entry.js');
+var traceur = require('traceur');
+var Entry = traceur.require(__dirname + '/../../app/models/entry.js');
 // var moment = require('moment');
 
 exports.trends = (req, res)=>{
-  // var userId = res.locals.user._id;
-  // Entry.findByUserId(userId, entries=>{
-  //   res.render('entires/trends', {entries:entries});
-  // });
+  var userId = res.locals.user._id;
+  Entry.findByUserId(userId, entries=>{
+    console.log(entries);
+    res.render('entries/trends');
+  });
 };
 
 exports.new = (req, res)=>{
