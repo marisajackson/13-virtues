f791eebaf6f019ebf9d8c07ca92e03651cc9c43f
Nathan Hood <nathanhood@me.com>
2014-06-04 23:21:50 -0500

    sample chart rendering

--- app/static/js/es6/chart.es6.js
+++ app/static/js/es6/chart.es6.js
@@ -1,4 +1,5 @@
 /* global AmCharts */
+/* jshint unused:false */
 
 (function() {
   'use strict';
@@ -9,12 +10,12 @@
 
 
 
-
+  var chartData = generateChartData();
   var chart = AmCharts.makeChart('chartdiv', {
     'type': 'serial',
     'theme': 'patterns',
     'pathToImages': 'http://www.amcharts.com/lib/3/images/',
-    'dataProvider': [],
+    'dataProvider': chartData,
     'valueAxes': [{
         'axisAlpha': 0.2,
         'dashLength': 1,
@@ -28,8 +29,8 @@
 		    'bulletColor':'#FFFFFF',
         'hideBulletsCount': 50,
         'title': 'red line',
-        'valueField': 'Average Rating',
-		'useLineColorForBulletBorder':true
+        'valueField': 'visits',
+		    'useLineColorForBulletBorder':true
     }],
     'chartScrollbar': {
         'autoGridCount': true,
@@ -39,54 +40,53 @@
     'chartCursor': {
         'cursorPosition': 'mouse'
     },
-    'categoryField': 'date',
-    'categoryAxis': {
+      'categoryField': 'date',
+      'categoryAxis': {
         'parseDates': true,
         'axisColor': '#DADADA',
         'dashLength': 1,
         'minorGridEnabled': true
     },
-	  'exportConfig':{
+	   'exportConfig':{
 	    menuRight: '20px',
       menuBottom: '30px',
       menuItems: [{
       icon: 'http://www.amcharts.com/lib/3/images/export.png',
       format: 'png'
       }]
-	  }
+	   }
   });
 
-  // chart.addListener('rendered', zoomChart);
-  // zoomChart();
+  chart.addListener('rendered', zoomChart);
+  zoomChart();
 
   // this method is called when chart is first inited as we listen for 'dataUpdated' event
-  // function zoomChart() {
-  //   // different zoom methods can be used - zoomToIndexes, zoomToDates, zoomToCategoryValues
-  //   chart.zoomToIndexes(chartData.length - 40, chartData.length - 1);
-  // }
+  function zoomChart() {
+    // different zoom methods can be used - zoomToIndexes, zoomToDates, zoomToCategoryValues
+    chart.zoomToIndexes(chartData.length - 40, chartData.length - 1);
+  }
 
 
-  // // generate some random data, quite different range
-  // function generateChartData() {
-  //   var chartData = [];
-  //   var firstDate = new Date();
-  //   firstDate.setDate(firstDate.getDate() - 5);
-  //
-  //   for (var i = 0; i < 1000; i++) {
-  //       // we create date objects here. In your data, you can have date strings
-  //       // and then set format of your dates using chart.dataDateFormat property,
-  //       // however when possible, use date objects, as this will speed up chart rendering.
-  //       var newDate = new Date(firstDate);
-  //       newDate.setDate(newDate.getDate() + i);
-  //
-  //       var visits = Math.round(Math.random() * (40 + i / 5)) + 20 + i;
-  //
-  //       chartData.push({
-  //           date: newDate,
-  //           visits: visits
-  //       });
-  //   }
-  //   return chartData;
-  // }
+// generate some random data, quite different range
+  function generateChartData() {
+    var chartData = [];
+    var firstDate = new Date();
+    firstDate.setDate(firstDate.getDate() - 5);
 
+    for (var i = 0; i < 1000; i++) {
+        // we create date objects here. In your data, you can have date strings
+        // and then set format of your dates using chart.dataDateFormat property,
+        // however when possible, use date objects, as this will speed up chart rendering.
+        var newDate = new Date(firstDate);
+        newDate.setDate(newDate.getDate() + i);
+
+        var visits = Math.round(Math.random() * (40 + i / 5)) + 20 + i;
+
+        chartData.push({
+            date: newDate,
+            visits: visits
+        });
+    }
+    return chartData;
+  }
 }());
